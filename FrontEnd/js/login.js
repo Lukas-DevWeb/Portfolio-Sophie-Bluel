b53fa01e59fd783b46c0602278a36605ef5ff72a
Leiiko <lukas.ls@outlook.fr>
2024-01-12 00:03:12 +0100

    Updates all files

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,65 +1,66 @@
-const loginForm = document.getElementById('login');
-const emailInput = document.querySelector('#email');
-const passwordInput = document.querySelector('#password');
-const url = 'http://localhost:5678/api/users/login';
-
-
-loginForm.addEventListener('submit', async (event) => {
-    // Empêche le formulaire de se soumettre
-    event.preventDefault();
-
-    // Vérifie que les champs sont remplis
-    if (emailInput.value === '' || passwordInput.value === '') {
-        alert('Veuillez remplir tous les champs.');
-        return;
-    }
-
-    // Vérifie si l'utilisateur est déjà connecté
-    if (window.sessionStorage.getItem('token') !== null) {
-        alert('Vous êtes déjà connecté');
-        return;
-    }
-
-    try {
-        // Envoi une requête de connexion
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: emailInput.value,
-                password: passwordInput.value,
-            })
-        });
-
-        // Vérifie si la connexion a réussi
-        if (response.status !== 200) {
-            const error = await response.json();
-            throw new Error(`Error: ${error.message}`);
-        }
-
-        // Enregistre le jeton de connexion dans la session
-        const result = await response.json();
-        const token = result.token;
-        window.sessionStorage.setItem('token', token);
-
-        // Redirige l'utilisateur vers la page d'accueil
-        window.location = 'index.html';
-
-        // Affiche un message de confirmation
-        alert('Connexion réussie !');
-
-    } catch (error) {
-        // Affiche un message d'erreur
-        errorMessage();
-    }
-});
-
-// Message d'erreur
-function errorMessage() {
-    const error = document.createElement('p');
-    error.textContent = 'Erreur dans l’identifiant ou le mot de passe';
-    error.setAttribute('id', 'errorMessage');
-    loginForm.appendChild(error);
+// Elements
+const elements = {
+    loginForm: document.getElementById("login-form"),
+    emailInput: document.getElementById("email"),
+    mdpInput: document.getElementById("mdp"),
+    errorMsg: document.querySelector(".erreur-msg"),
+  };
+  
+  // State
+  let userLogins = {
+    email: "",
+    password: "",
+  };
+  
+  // Fonction Fetch POST pour le Login
+  function fetchPost(userLogins) {
+    fetch(`http://localhost:5678/api/users/login`, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userLogins),
+      mode: "cors",
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        handleLoginResponse(res);
+      })
+      .catch((err) => {
+        console.error("Erreur de Fetch: ", err);
+        elements.errorMsg.innerText = "Erreur lors de la connexion";
+      });
   }
+  
+  // Fonction de gestion de la réponse du login
+  function handleLoginResponse(res) {
+    if (res.message === "user not found" || localStorage.token === "undefined") {
+      elements.errorMsg.innerText = "Erreur dans l’identifiant ou le mot de passe";
+      console.log("Connexion Impossible : Erreur Identifiant ou Mot de passe");
+    } else {
+      localStorage.token = res.token;
+      localStorage.login = true;
+      window.location.href = "index.html";
+      console.log("Connexion réussie");
+    }
+  }
+  
+  // Écouteur pour la saisie de l'email
+  elements.emailInput.addEventListener("input", (e) => {
+    userLogins.email = e.target.value;
+    console.log(e.target.value);
+  });
+  
+  // Écouteur pour la saisie du mot de passe
+  elements.mdpInput.addEventListener("input", (e) => {
+    userLogins.password = e.target.value;
+    console.log(e.target.value);
+  });
+  
+  // Appel fetch lors de la soumission du formulaire
+  elements.loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(userLogins);
+    fetchPost(userLogins);
+  });
--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,171 +1,309 @@
-// Url Api 
-const urlApi = "http://localhost:5678/api/";
+// Const
+const gallery = document.querySelector(".gallery");
+const filtersContainer = document.querySelector(".filters-container");
+const editBanner = document.querySelector(".modify-banner");
+const editBtn = document.querySelectorAll(".edit-btn");
+const modalContainer = document.querySelector(".modal-container");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
+const modalGallery = document.querySelector(".modal-gallery-work");
+const header = document.querySelector("header");
+const portfolio = document.getElementById("portfolio");
+const log = document.querySelector(".log-link-title");
+const modal1 = document.querySelector(".pictures-gallery");
+const modal2 = document.querySelector(".add-picture-gallery");
+const addPictureBtn = document.querySelector(".addPicture-btn");
+const returnArrow = document.querySelector(".return-arrow");
+const addPicture = document.querySelector(".addPictures");
+const addImageModal = document.querySelector(".btn-addImage");
+const validateBtn = document.querySelector(".validate-btn");
+const addTitle = document.getElementById("add-title");
+const addCategorie = document.getElementById("add-categories");
+const previewImg = document.querySelector(".preview-img");
+const imgContainer = document.querySelector(".img-container");
+const errorAdd = document.querySelector(".error-add");
+const deleteMsg = document.querySelector(".delete-msg");
+const filters = new Set();
+// Let
+let tokenValue = localStorage.token;
+let imageForm = "";
+let categoryForm = "";
+let titleForm;
 
-// Liste des travaux (initialisée à vide)
-let works = [];
+// Fetch Works
+const getWorks = async (categorieId = null) => {
+  // lien avec l'API
+  await fetch("http://localhost:5678/api/works")
+    .then((res) => res.json())
+    .then((works) => {
+       // Ajout des travaux
+      createWorks(works,categorieId);
+      createWorksModale(works);
+     
+    })    
+    .catch((err) => {
+      console.log(`Erreur : ${err}`);
+    });
+};
 
+// Fetch Catégories
+const getCategories = async () => {
+  await fetch("http://localhost:5678/api/categories")
+  .then((res) => res.json())
+  .then((categories) => {
+    //Ajout des filtres
+    createCategories(categories);
+  })  
+  .catch((err) => {
+    console.log(`Erreur : ${err}`);
+  });
+};
 
-async function allWorks() {
-    try {
-        const response = await fetch(urlApi + 'works', {
-            method: 'GET',
-            headers: {
-                'accept': 'application/json'
-            }
-        });
+// Fetch suppression travaux
+const fetchDelete = async (id) => {
+  await fetch("http://localhost:5678/api/works/" + id, {
+    method: "DELETE",
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${tokenValue}`,
+    },
+    mode: "cors",
+  })
+    .then((response) => response.json())
+    .then((res) => {
+      if (res.confirmation === "OK") {
+        id.remove();
+      }
+      console.log(res);
+    })
+    .catch((err) => console.log("Il y a eu une erreur sur le Fetch: " + err));
+};
 
-        if (!response.ok) {
-            throw new Error('Unexpected Error');
-        }
-
-        // Extrait les données JSON de la réponse
-        const data = await response.json();
-
-        // Stocke les travaux dans la variable "works"
-        works = data;
-        window.localStorage.setItem('works', JSON.stringify(works));
-
-        generateGallery(works);
-
-        // Retourne les travaux
-        return works;
-
-    } catch (error) {
-
-        // Affiche une alerte avec le message d'erreur
-        alert(error.message);
-        throw error;
+// Implémente les Images dans la gallery
+function createWorks(works, categorieId) {
+  works.map((work) => {
+    if (categorieId == work.category.id || categorieId == null) {
+      const post = document.createElement("figure");
+      post.setAttribute("id", `${work.id}.`);
+      post.innerHTML = `
+      <img src=${work.imageUrl} alt="image de ${work.title}">
+      <figcaption>${work.title}</figcaption> 
+      `;
+      gallery.appendChild(post);
     }
-
+  });
 }
 
-// Quand l'utilisateur est connecté 
-async function adminLogin() {
-    if (window.sessionStorage.getItem('token') === null) {
-        categoriesButtons();
+// Créer les filtres
+function createCategories(categories) {
+  categories.map((filter) => {
+    filters.add(filter.name);
+  });
+
+  // Tranforme l'objet set en array
+  const filtersArray = Array.from(filters);
+  //Créer les éléments buttons
+  for (let i = 0; i < categories.length; i++) {
+    const filtre = document.createElement("button");
+    filtre.classList.add(
+      "filter-btn"     
+    );
+    filtre.innerText = categories[i].name;
+    filtre.setAttribute("categorieId", categories[i].id);
+    filtersContainer.appendChild(filtre);
+  }
+
+  //Filtre au clic
+  //Filtre pour le bouton Tous
+  const btnTous = document.getElementById("tous");
+  btnTous.addEventListener("click", () => {
+    gallery.innerHTML = "";
+    getWorks();
+  });
+
+  // Filtre pour les catégories suivantes
+  const buttons = document.querySelectorAll(".filter-btn");
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      buttons.forEach((button) => button.classList.remove("active"));
+      gallery.innerHTML = "";
+      /*const workFiltered = works.filter((work) => {
+        return work.category.name === e.target.innerText;
+      });*/
+      button.classList.add("active");
+    
+      let categorieId = button.getAttribute("categorieId");
+      getWorks(categorieId);
+    });
+  });
+}
+
+
+
+// Ajout de la gallery dans la modale
+function createWorksModale(works) {
+  works.map((work) => {
+    const workPost = document.createElement("figure");
+    workPost.setAttribute("id", `${work.id}`);
+    workPost.innerHTML = `
+    <div class="workgallery-container">
+      <i id="${work.id}"  class="fa-solid fa-trash-can trash-icon" ></i>
+      <img class="modal-image" src=${work.imageUrl} alt="image de ${work.title}">
+    </div> 
+    `;
+    modalGallery.appendChild(workPost);
+
+    deleteImage(workPost);
+  });
+}
+//Affiche le mode edition si connecté
+function editMode() {
+  if (localStorage.login === "true") {
+    filtersContainer.style.setProperty("visibility", "hidden");
+    header.style.setProperty("margin-top", "100px");
+    portfolio.style.setProperty("margin-top", "150px");
+    editBanner.style.setProperty("display", "flex");
+    log.innerText = "logout";
+    editBtn.forEach((btn) => {
+      btn.style.setProperty("display", "flex");
+    });
+    console.log("Vous êtes connecté ! Enjoy !");
+  } else {
+    console.log("Vous n'êtes pas connecté ! Identifiez-vous !");
+  }
+}
+
+// Au clic sur "logout", supprime dans le local storage login: true et token
+log.addEventListener("click", () => {
+  localStorage.removeItem("login");
+  localStorage.removeItem("token");
+  log.innerText = "login";
+});
+
+// Affiche la modale
+function toggleModal() {
+  modalContainer.classList.toggle("target");
+}
+modalTriggers.forEach((trigger) =>
+  trigger.addEventListener("click", toggleModal)
+);
+
+function showAddPictureModal() {
+  modal1.style.display = "none";
+  modal2.style.display = "flex";
+}
+
+addPictureBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  showAddPictureModal();
+});
+
+//Au click sur la fleche retour de la modal on revient à la modale précédente
+
+function returnModal1() {
+  modal1.style.display = "block";
+  modal2.style.display = "none";
+  previewImg.src = "";
+  previewImg.style.setProperty("display", "none");
+  imgContainer.style.setProperty("display", "flex");
+}
+
+returnArrow.addEventListener("click", returnModal1);
+
+//Function suppression des images
+function deleteImage(imgValue) {
+  const deleteIcon = document.querySelectorAll(".trash-icon");
+  deleteIcon.forEach((delIcon) => {
+    delIcon.addEventListener("click", (e) => {
+      e.preventDefault();
+      const idRemove = document.getElementById(e.target.id);
+      const portfolioRemove = document.getElementById(e.target.id + ".");
+      fetchDelete(parseInt(e.target.id));
+      console.log(e.target.id);
+      idRemove.remove();
+      portfolioRemove.remove();
+      deleteMsg.innerText = "Supprimé !";
+      setTimeout(() => {
+        deleteMsg.innerText = "";
+      }, 3000);
+    });
+  });
+}
+
+//Function ajout des images
+function addImage() {
+  // Image
+  addImageModal.addEventListener("input", (e) => {
+    //console.log(addImageModal.files[0]);
+    imageForm = e.target.files[0];
+    const img = URL.createObjectURL(imageForm);
+    previewImg.src = img;
+    previewImg.style.setProperty("display", "block");
+    imgContainer.style.setProperty("display", "none");
+  });
+  //Titre
+  addTitle.addEventListener("input", (e) => {
+    titleForm = e.target.value;
+  });
+  //Catégories
+  addCategorie.addEventListener("input", (e) => {
+    categoryForm = e.target.selectedIndex;
+  });
+  //Submit
+  addPicture.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (imageForm && titleForm && categoryForm) {
+      const formData = new FormData();
+      console.log(imageForm, titleForm, categoryForm);
+      formData.append("image", imageForm);
+      formData.append("title", titleForm);
+      formData.append("category", categoryForm);
+      console.log(formData.entries());
+      //Fetch ajout des travaux
+      fetch("http://" + window.location.hostname + ":5678/api/works", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${tokenValue}`,
+        },
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          console.log(res);
+          errorAdd.innerText = "Posté !";
+          errorAdd.style.color = "green";
+          //Clear les galleries
+          gallery.innerHTML = "";
+          modalGallery.innerHTML = "";
+          getWorks();
+          addPicture.reset();
+          previewImg.src = "";
+          previewImg.style.setProperty("display", "none");
+          imgContainer.style.setProperty("display", "flex");
+          setTimeout(() => {
+            errorAdd.innerText = "";
+          }, 4000);
+        })
+        .catch((err) =>
+          console.log("Il y a eu une erreur sur le Fetch: " + err)
+        );
     } else {
-        logout();
-        topBlackMenu();
-        modifyProjets();
-        modifyButton();
+      errorAdd.innerText = "Veuillez remplir tous les champs.";
+      errorAdd.style.color = "red";
+      setTimeout(() => {
+        errorAdd.innerText = "";
+      }, 4000);
+      console.log("Tous les champs ne sont pas remplis !");
     }
+  });
 }
 
-// Catégories
-async function categoriesButtons() {
-
-    const response = await fetch(urlApi + 'categories',
-        {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-    // H2 : Mes projets
-    const portfolio = document.querySelector('#portfolio');
-    const myProject = document.createElement('h2');
-    myProject.textContent = 'Mes Projets';
-    const categories = await response.json();
-
-    // Filtres Catégories
-    const filtersButtons = document.createElement('div');
-    filtersButtons.classList.add('categories');
-    const categoriesButtons = document.createElement('button');
-    categoriesButtons.textContent = 'Tous';
-    categoriesButtons.classList.add('btn', 'all');
-    filtersButtons.appendChild(categoriesButtons);
-    portfolio.prepend(filtersButtons);
-    portfolio.prepend(myProject);
-
-    for (let categorie of categories) {
-        //console.log(categorie);
-        const button = document.createElement('button');
-        button.setAttribute('class', 'btn');
-        button.setAttribute('data-category-id', `${categorie.id}`);
-        button.textContent = `${categorie.name}`;
-        //console.log(button.dataset.categoryId);
-        filtersButtons.appendChild(button);
-    }
-
-    let buttons = document.querySelectorAll('.btn');
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', showCategories);
-    }
-
+function main(){
+  //Appel des différentes fonctions
+  getWorks();
+  getCategories();
+  editMode();
+  addImage();
 }
 
-// Filtre à travers les catégories
-function showCategories(event) {
-    const works = JSON.parse(localStorage.getItem('works')); // recupère tableau works
-    if (event.target.nodeName === 'BUTTON' && event.target.className === 'btn all') {
-        allWorks();
-    } else if (event.target.nodeName === 'BUTTON' && event.target.dataset.categoryId) {
-        const workCategory = works.filter(i => i.categoryId == `${event.target.dataset.categoryId}`);
-        generateGallery(workCategory);
-    }
-}
-
-// Génère les projets
-function generateGallery(works) {
-    const gallery = document.querySelector('.gallery');
-    gallery.innerHTML = '';
-    for (let projet of works) {
-        gallery.innerHTML += `<figure class="figureworks">
-		 				<img src="${projet.imageUrl}" alt="${projet.title}">
-		 				<figcaption>${projet.title}</figcaption>`;
-    }
-}
-
-// Id Login - Logout
-function logout() {
-    const logoutElement = document.getElementById("logout");
-    logoutElement.textContent = "logout";
-    logoutElement.addEventListener('click', () => {
-        window.sessionStorage.removeItem('token');
-        alert('Vous avez été déconnecté.');
-    });
-}
-
-// Générateur d'éléments DOM
-function elementGenerator(type, text, attributes = []) {
-    let element = document.createElement(type);
-    if (text) {
-        element.textContent = text;
-    }
-    attributes.forEach(attribute => {
-        const [name, value] = attribute.split('=');
-        element.setAttribute(name, value);
-    });
-    return element;
-}
-
-// Top édition noir
-function topBlackMenu() {
-    const div = elementGenerator('div', undefined, ['class=publish']);
-    const modifyLink = elementGenerator('a', undefined, ['href=#']);
-    const span = elementGenerator('span', 'Mode édition', []);
-    const icon = elementGenerator('i', undefined, ['class=fa-regular fa-pen-to-square']);
-    div.appendChild(modifyLink);
-    document.querySelector('body').prepend(div);
-    modifyLink.appendChild(icon);
-    modifyLink.appendChild(span);
-    div.appendChild(button);
-}
-
-// 'Mes projets' avec Lien Modifier 
-function modifyProjets() {
-    const portfolio = document.querySelector('#portfolio');
-    const myProject = document.createElement('h2');
-    myProject.textContent = 'Mes Projets';
-    const aLink = elementGenerator('a', undefined, ['href=#modal1']);
-    const iElement = elementGenerator('i', undefined, ['class=fa-regular fa-pen-to-square']);
-    const spanElement = elementGenerator('span', 'modifier', []);
-    aLink.appendChild(iElement);
-    aLink.appendChild(spanElement);
-    portfolio.prepend(myProject);
-    myProject.appendChild(aLink);
-}
-
-allWorks();
-adminLogin();
+main();
