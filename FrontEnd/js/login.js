7dcf2da7fa7e3aeeac0ef0ee7a7496f34219226f
Leiiko <lukas.ls@outlook.fr>
2024-03-05 17:09:27 +0100

    Update
    modification du fichier login.html et login.js pour que le message s'affiche qu'une seule fois.

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,29 +1,27 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', async (e) => {
-	const email = document.getElementById('email').value;
-	const password = document.getElementById('password').value;
-	e.preventDefault();
-	try {
-	const response = await fetch('http://localhost:5678/api/users/login', {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({email, password}),
-});
-	if (response.ok) {
-		const data = await response.json();
-		sessionStorage.setItem('accessToken', data.token);
-		window.location.href = './index.html';
-	} else {
-		const connexion = document.querySelector('div');
-		const error = document.createElement('p');
-		error.innerText = `"Erreur dans l’identifiant ou le mot de passe"`;
-		error.style.textAlign = 'center';
-		error.style.color = 'red';
-		error.style.marginBottom = '15px';
-		connexion.insertBefore(error, connexion.lastElementChild);
-	}
-}
-catch (error) {
-	console.log(error)
-}
+const messageError = document.querySelector(".message-error");
+const form = document.querySelector("form");
+form.addEventListener("submit", async (e) => {
+  try {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const response = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    if (response.ok) {
+      const data = await response.json();
+      sessionStorage.setItem("accessToken", data.token);
+      window.location.href = "./index.html";
+    } else {
+      messageError.innerText = "Erreur dans l’identifiant ou le mot de passe";
+	  messageError.style.textAlign = 'center';
+	  messageError.style.color = 'red';
+	  messageError.style.marginBottom = '15px';
+    }
+  } catch (error) {
+    console.log(error);
+    messageError.innerText = "Suite à un problème technique, veuillez réessayer ultérieurement";
+  }
 });
